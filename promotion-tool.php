2c8c4fb49eecdf609ff97c43d3fb29bc1b1499f0
moonshotdigitals <asim.moonshot@gmail.com>
2025-07-09 11:21:58 +0500

    Version Update : 1.0.17

--- promotion-tool.php
+++ promotion-tool.php
@@ -9,14 +9,14 @@
  * that starts the plugin.
  *
  * @link              https://themoonshot.digital
- * @since             1.0.16
+ * @since             1.0.17
  * @package           Promotion_Tool
  *
  * @wordpress-plugin
  * Plugin Name:       Promotion Tool
  * Plugin URI:        https://themoonshot.digital
  * Description:       Incentive higher quantity purchases. Increasing sales.
- * Version:           1.0.16
+ * Version:           1.0.17
  * Author:            Moonshot
  * Author URI:        https://themoonshot.digital
  * License:           GPL-2.0+
@@ -32,10 +32,10 @@ if ( ! defined( 'WPINC' ) ) {
 
 /**
  * Currently plugin version.
- * Start at version 1.0.16 and use SemVer - https://semver.org
+ * Start at version 1.0.17 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'PROMOTION_TOOL_VERSION', '1.0.16' );
+define( 'PROMOTION_TOOL_VERSION', '1.0.17' );
 
 /**
  * The code that runs during plugin activation.
@@ -81,7 +81,7 @@ require plugin_dir_path( __FILE__ ) . 'includes/class-promotion-tool.php';
  * then kicking off the plugin from this point in the file does
  * not affect the page life cycle.
  *
- * @since    1.0.16
+ * @since    1.0.17
  */
 function run_promotion_tool() {
 
